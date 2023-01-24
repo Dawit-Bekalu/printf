0789c5a1f130d410cf3a3300da46481dcba97181
Dawit Bekalu <83080747+Dawit-Bekalu@users.noreply.github.com>
2023-01-25 01:08:58 +0400

    Hello team

--- main.h
+++ main.h
@@ -4,8 +4,10 @@
 #include <stdio.h>
 #include <unistd.h>
 
+
 #define UNUSED(x) (void)(x)
 #define BUFF_SIZE 1024
+
 
 /* FLAGS */
 #define F_MINUS 1
@@ -14,9 +16,11 @@
 #define F_HASH 8
 #define F_SPACE 16
 
+
 /* SIZES */
 #define S_LONG 2
 #define S_SHORT 1
+
 
 /**
  * struct fmt - Struct op
@@ -30,7 +34,6 @@ struct fmt
 	int (*fn)(va_list, char[], int, int, int, int);
 };
 
-
 /**
  * typedef struct fmt fmt_t - Struct op
  * typedefination
@@ -42,8 +45,6 @@ typedef struct fmt fmt_t;
 int _printf(const char *format, ...);
 int handle_print(const char *fmt, int *i,
 va_list list, char buffer[], int flags, int width, int precision, int size);
-
-/********************* FUNCTIONS *********************/
 
 /* Funtions to print chars and strings */
 int print_char(va_list types, char buffer[],
