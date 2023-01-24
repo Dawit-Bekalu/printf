1ae333d8850cb202aedb0e198aba540af6152db1
Dawit Bekalu <83080747+Dawit-Bekalu@users.noreply.github.com>
2023-01-25 01:02:50 +0400

    Hello team

--- functions2.c
+++ functions2.c
@@ -1,6 +1,4 @@
 #include "main.h"
-
-/********************* PRINT POINTER *********************/
 /**
  * print_pointer - Prints the value of a pointer variable
  * to the
@@ -53,8 +51,6 @@ int print_pointer(va_list types, char buffer[],
 	return (write_pointer(buffer, ind, length,
 		width, flags, padd, extra_c, padd_start));
 }
-
-/************************* PRINT NON PRINTABLE *************************/
 /**
  * print_non_printable - Prints ascii codes in hexa of non printable chars
  * @types: Lista of arguments
@@ -93,8 +89,6 @@ int print_non_printable(va_list types, char buffer[],
 
 	return (write(1, buffer, i + offset));
 }
-
-/************************* PRINT REVERSE *************************/
 /**
  * print_reverse - Prints reverse string.
  * @types: Lista of arguments
@@ -137,7 +131,6 @@ int print_reverse(va_list types, char buffer[],
 	}
 	return (count);
 }
-/************************* PRINT A STRING IN ROT13 *************************/
 /**
  * print_rot13string - Print a string in rot13.
  * @types: Lista of arguments
