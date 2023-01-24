94cf0a5b07e4118c97f9991d068e2e26bcfdb768
robela_bella <110679985+robelabell9@users.noreply.github.com>
2023-01-25 01:31:14 +0400

    hi

--- utils.c
+++ utils.c
@@ -47,6 +47,7 @@ int is_digit(char c)
 
 	return (0);
 }
+
 /**
  * convert_size_number - Casts a number to the specified size
  * @num: Number to be casted.
@@ -60,7 +61,6 @@ long int convert_size_number(long int num, int size)
 		return (num);
 	else if (size == S_SHORT)
 		return ((short)num);
-
 	return ((int)num);
 }
 /**
