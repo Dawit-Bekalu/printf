8510b7f08caa2ce199cde7cf1207e74d7f720e3d
Dawit Bekalu <83080747+Dawit-Bekalu@users.noreply.github.com>
2023-01-25 01:00:28 +0400

    Hello team

--- functions.c
+++ functions.c
@@ -1,7 +1,5 @@
 #include "main.h"
 
-/************************* PRINT CHAR ****************************/
-
 /**
  * print_char - Prints a char
  * to the standard
@@ -21,7 +19,6 @@ int print_char(va_list types, char buffer[],
 
 	return (handle_write_char(c, buffer, flags, width, precision, size));
 }
-/************************* PRINT A STRING *************************/
 /**
  * print_string - Prints a string
  * @types: List a of arguments
@@ -76,7 +73,6 @@ int print_string(va_list types, char buffer[],
 
 	return (write(1, str, length));
 }
-/************************* PRINT PERCENT SIGN *************************/
 /**
  * print_percent - Prints a percent sign
  * @types: Lista of arguments
@@ -98,8 +94,6 @@ int print_percent(va_list types, char buffer[],
 	UNUSED(size);
 	return (write(1, "%%", 1));
 }
-
-/************************* PRINT INT *************************/
 /**
  * print_int - Print int
  * @types: Lista of arguments
@@ -142,8 +136,6 @@ int print_int(va_list types, char buffer[],
 
 	return (write_number(is_negative, i, buffer, flags, width, precision, size));
 }
-
-/************************* PRINT BINARY *************************/
 /**
  * print_binary - Prints an unsigned number
  * @types: Lista of arguments
